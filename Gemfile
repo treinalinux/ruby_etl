4680c771ab920b9cb65e4f9377edf7886f582512
Alan da Silva Alves <linuxfama@gmail.com>
2021-10-24 16:35:47 -0300

    feat: params fonud

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.2'
 
 gem 'nokogiri', '~> 1.12', '>= 1.12.5'
+gem 'httparty', '~> 0.20.0'
--- quotes.rb
+++ quotes.rb
@@ -3,21 +3,11 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'httparty'
 
 quotes_url = Nokogiri::HTML5(URI.open('http://quotes.toscrape.com/'))
 
-# Eu preciso da frase:
-#
-puts quotes_url.css('div')[10].css('span')[0].text
-
-# Eu preciso do autor:
-#
-puts quotes_url.css('div')[10].css('span')[1].css('small').text
-
-# Eu preciso do link do autor:
-#
-puts quotes_url.css('div')[10].css('a')[0]['href']
-
-# Eu preciso das tags:
-#
-puts quotes_url.css('div')[10].css('meta')
+puts quotes_url.css('span.text').text
+puts quotes_url.css('span small.author').text
+puts "https://quotes.toscrape.com#{quotes_url.css('span a').attribute('href').value}"
+puts quotes_url.css('div.tags meta.keywords').attribute('content').value.split(',')
